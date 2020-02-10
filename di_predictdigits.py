114f43f90e9441ffbc84b649ab5efa8983cc4dcd
llandroojeda <llandroojeda@gmail.com>
2020-02-10 12:38:43 -0600

    intial commmit

---
+++ di_predictdigits.py
@@ -0,0 +1,30 @@
+from keras.models import load_model
+from tkinter import *
+import tkinter as tk
+import win32gui
+from PIL import ImageGrab, Image
+import numpy as np
+
+
+
+
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.x = self.y = 0
+
+        self.canvas = tk.Canvas(self, width=300, height=300, bg = "white", cursor="cross")
+        self.label = tk.Label(self, text="Thinking..", font=("Helvetica", 48))
+        self.classify_btn = tk.Button(self, text = "Recognise", command =         self.classify_handwriting)
+        self.button_clear = tk.Button(self, text = "Clear", command = self.clear_all)
+
+
+
+    def classify_handwriting(self):
+        HWND = self.canvas.winfo_id()  # get the handle of the canvas
+        rect = win32gui.GetWindowRect(HWND)  # get the coordinate of the canvas
+        im = ImageGrab.grab(rect)
+
+app = App()
+mainloop()
